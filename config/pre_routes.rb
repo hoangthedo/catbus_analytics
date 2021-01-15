b94efaf9aeb65b9d8816635aee35299b7201d8ce
hoangthedo <hoang@cohota.com>
2021-01-15 13:24:51 +0700

    change routes

--- config/pre_routes.rb
+++ config/pre_routes.rb
@@ -1,15 +1,15 @@
 CanvasRails::Application.routes.draw  do
     scope(controller: :catbus_analytics) do
-        get "/accounts/:account_id/analytics", action: :index, as: :analytics
-        get "/accounts/:account_id/analytics/root_accounts", action: :accounts
-        get "/accounts/:account_id/analytics/root_accounts/:root_account_id", action: :show
+        get "/accounts/:account_id/analytics-v2", action: :index, as: :catbus_analytics
+        get "/accounts/:account_id/analytics-v2/root_accounts", action: :accounts
+        get "/accounts/:account_id/analytics-v2/root_accounts/:root_account_id", action: :show
     end
     
     ApiRouteSet::V1.draw(self) do
         scope(controller: :catbus_analytics) do
-            get "/accounts/:account_id/analytics", action: :index, as: :analytics
-            get "/accounts/:account_id/analytics/root_accounts", action: :accounts
-            get "/accounts/:account_id/analytics/root_accounts/:root_account_id", action: :show
+            get "/accounts/:account_id/analytics-v2", action: :index, as: :catbus_analytics
+            get "/accounts/:account_id/analytics-v2/root_accounts", action: :accounts
+            get "/accounts/:account_id/analytics-v2/root_accounts/:root_account_id", action: :show
         end
     end
 end
